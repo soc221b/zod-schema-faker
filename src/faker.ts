0b29ca569b2f65d2edf7d278e764001c48b9fae6
Ernest <iendeavor.github.io@gmail.com>
2022-12-04 11:12:24 +0800

    refactor: use workaround for seed

--- src/faker.ts
+++ src/faker.ts
@@ -1,28 +1,20 @@
 import { Faker, faker as _faker } from '@faker-js/faker'
 import RandExp from 'randexp'
 
-let _seedValue: number | number[] | undefined
-let _shouldSeed: boolean = false
+let _seedValue: number | undefined
 
 // https://github.com/faker-js/faker/issues/448
 // TODO: create standalone faker instead of use following workaround
 export const runFake = <Runner extends (faker: Faker) => any>(runner: Runner): ReturnType<Runner> => {
-  let oldSeedValue
-  if (_shouldSeed) {
-    oldSeedValue = _faker.seedValue
-    _faker.seed(_seedValue)
-  }
+  const oldSeedValue = _faker.seed()
+  _faker.seed(_seedValue)
 
   const result = runner(_faker)
   if (result instanceof Promise) {
     throw new SyntaxError('InternalError: runFake cannot be used with async functions')
   }
 
-  if (_shouldSeed) {
-    _faker.seed(oldSeedValue)
-  }
-
-  _shouldSeed = false
+  _faker.seed(oldSeedValue)
 
   return result
 }
@@ -36,7 +28,6 @@ export const randexp = (pattern: string | RegExp, flags?: string) => {
 /**
  * sets the seed to use.
  */
-export const seed = (value?: number | number[]): void => {
+export const seed = (value?: number): void => {
   _seedValue = value
-  _shouldSeed = true
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-export { fake } from './fake'
+import { z } from 'zod'
+import { fake as _fake } from './fake'
+import { seed } from './faker'
+
+export const fake = <T extends z.ZodType>(schema: T): z.infer<T> => {
+  const result = _fake(schema)
+  seed()
+  return result
+}
+
 export { seed } from './faker'
 export { install } from './zod-type-kind-to-zod-type-faker'
 export { ZodSchemaFakerError } from './error'
