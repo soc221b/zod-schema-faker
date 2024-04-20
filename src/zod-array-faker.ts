f7a2315a6c1b3ea972a94cbdecad09721e0dd62a
Ernest <iendeavor.github.io@gmail.com>
2024-04-20 21:01:21 +0800

    fix: array

--- src/zod-array-faker.ts
+++ src/zod-array-faker.ts
@@ -5,8 +5,11 @@ import { ZodTypeFaker } from './zod-type-faker'
 
 export class ZodArrayFaker<T extends z.ZodArray<any, any>> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
-    let min = this.schema._def.minLength?.value ?? 0
-    let max = this.schema._def.maxLength?.value ?? runFake(faker => faker.number.int({ min, max: min + 2 }))
+    let min = this.schema._def.exactLength?.value ?? this.schema._def.minLength?.value ?? 0
+    let max =
+      this.schema._def.exactLength?.value ??
+      this.schema._def.maxLength?.value ??
+      runFake(faker => faker.number.int({ min, max: min + 2 }))
 
     return Array(runFake(faker => faker.number.int({ min, max })))
       .fill(null)
