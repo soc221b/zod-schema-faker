e3ceca731521e433bd3d7f9eaaf54d1bba77be57
Ernest <iendeavor.github.io@gmail.com>
2024-04-20 22:29:50 +0800

    feat: discriminated union

--- src/zod-discriminated-union-faker.ts
+++ src/zod-discriminated-union-faker.ts
@@ -3,15 +3,19 @@ import { fake } from './fake'
 import { runFake } from './faker'
 import { ZodTypeFaker } from './zod-type-faker'
 
-export class ZodDiscriminatedUnionFaker<T extends z.ZodDiscriminatedUnion<any, any, any>> extends ZodTypeFaker<T> {
+export class ZodDiscriminatedUnionFaker<T extends z.ZodDiscriminatedUnion<any, any>> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
-    const options = Array.from(this.schema._def.options.values())
-    const randomIndex = runFake(faker => faker.number.int({ min: 0, max: options.length - 1 }))
+    const options = Array.from(
+      typeof this.schema._def.options.values === 'function'
+        ? this.schema._def.options.values()
+        : this.schema._def.options.values,
+    )
+    const randomIndex = runFake(faker => faker.number.int({ min: 0, max: Math.max(0, options.length - 1) }))
     const randomSchema = options[randomIndex]
-    return fake(randomSchema)
+    return fake(randomSchema as any)
   }
 
-  static create<T extends z.ZodDiscriminatedUnion<any, any, any>>(schema: T): ZodDiscriminatedUnionFaker<T> {
+  static create<T extends z.ZodDiscriminatedUnion<any, any>>(schema: T): ZodDiscriminatedUnionFaker<T> {
     return new ZodDiscriminatedUnionFaker(schema)
   }
 }
