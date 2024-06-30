10810acb414b5b8ce52f62172ab934585ca74ff8
Ernest <>
2024-06-30 16:23:18 +0800

    feat: throw better errors

--- src/fake.ts
+++ src/fake.ts
@@ -14,7 +14,9 @@ export const fake = <T extends z.ZodType>(schema: T): z.infer<T> => {
   const typeName = (schema._def as any).typeName
   const faker = zodTypeToZodTypeFaker.get(schema) ?? zodTypeKindToZodTypeFaker.get(typeName)
   if (faker === undefined) {
-    throw new ZodSchemaFakerError(`Unsupported schema: ${typeName}`)
+    throw new ZodSchemaFakerError(`Unsupported schema type: ${typeName}.
+- If this is a custom schema, you may not have installed a corresponding faker. If you need help with installation, please refer to the documentation for more information.
+- If this is a built-in schema, it may not be implemented yet. Please file an issue to let us know: https://github.com/soc221b/zod-schema-faker/issues`)
   }
 
   return new faker(schema).fake()
