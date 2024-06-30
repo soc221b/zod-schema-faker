fdf778e024e69671e9ffbb678e378e88db897019
Ernest <>
2024-06-30 16:49:30 +0800

    refactor: generate more data for map

--- src/zod-map-faker.ts
+++ src/zod-map-faker.ts
@@ -1,10 +1,26 @@
 import * as z from 'zod'
 import { fake } from './fake'
 import { ZodTypeFaker } from './zod-type-faker'
+import { runFake } from './faker'
 
 export class ZodMapFaker<T extends z.ZodMap<any, any>> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
-    return new Map([[fake(this.schema._def.keyType), fake(this.schema._def.valueType)]])
+    let i = 0
+    const min = 0
+    const max = 10
+    const result: z.infer<T> = new Map()
+    while (++i < min) {
+      result.set(fake(this.schema._def.keyType), fake(this.schema._def.valueType))
+    }
+    while (++i < max) {
+      result.set(fake(this.schema._def.keyType), fake(this.schema._def.valueType))
+
+      if (runFake(faker => faker.datatype.boolean())) {
+        break
+      }
+    }
+
+    return result
   }
 
   static create<T extends z.ZodMap<any, any>>(schema: T): ZodMapFaker<T> {
