5a4fca0dc9e97ea3178b8102dec8337b4fe98583
Ernest <>
2024-06-30 16:45:55 +0800

    refactor: generate more data for record

--- src/zod-record-faker.ts
+++ src/zod-record-faker.ts
@@ -1,10 +1,24 @@
 import * as z from 'zod'
 import { fake } from './fake'
 import { ZodTypeFaker } from './zod-type-faker'
+import { runFake } from './faker'
 
 export class ZodRecordFaker<T extends z.ZodRecord<any, any>> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
-    return { [fake(this.schema._def.keyType)]: fake(this.schema._def.valueType) }
+    let i = 0
+    const min = 0
+    const max = 10
+    const result: Record<any, any> = {}
+    while (++i < min) {
+      result[fake(this.schema._def.keyType)] = fake(this.schema._def.valueType)
+    }
+    while (++i < max) {
+      result[fake(this.schema._def.keyType)] = fake(this.schema._def.valueType)
+      if (runFake(faker => faker.datatype.boolean())) {
+        break
+      }
+    }
+    return result
   }
 
   static create<T extends z.ZodRecord<any, any>>(schema: T): ZodRecordFaker<T> {
