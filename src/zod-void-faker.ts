ea67c88ac1113ceb7989df1652d53843257c420e
Ernest <>
2024-08-16 22:41:08 +0800

    fix: it should return `undefined` when fake z.void

--- src/zod-void-faker.ts
+++ src/zod-void-faker.ts
@@ -4,7 +4,7 @@ import { ZodTypeFaker } from './zod-type-faker'
 
 export class ZodVoidFaker extends ZodTypeFaker<z.ZodVoid> {
   fake(): z.infer<z.ZodVoid> {
-    return fake(z.any())
+    return undefined
   }
 
   static create(schema: z.ZodVoid): ZodVoidFaker {
