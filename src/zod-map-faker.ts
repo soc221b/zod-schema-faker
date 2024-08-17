bc73d8fb9f549ed5fc0d98d1c8f4813f0026ac84
Ernest <22850435+soc221b@users.noreply.github.com>
2024-08-17 10:52:40 +0800

    refactor: remove dead code (#212)
    Co-authored-by: Ernest <>

--- src/zod-map-faker.ts
+++ src/zod-map-faker.ts
@@ -6,12 +6,8 @@ import { runFake } from './faker'
 export class ZodMapFaker<T extends z.ZodMap<any, any>> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
     let i = 0
-    const min = 0
     const max = 10
     const result: z.infer<T> = new Map()
-    while (++i < min) {
-      result.set(fake(this.schema._def.keyType), fake(this.schema._def.valueType))
-    }
     while (++i < max) {
       result.set(fake(this.schema._def.keyType), fake(this.schema._def.valueType))
 
