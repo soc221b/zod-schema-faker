862ae52dc7124f0b7953651dcdb2af2035c106aa
Ernest <soc221b.e@gmail.com>
2025-01-11 11:11:33 +0800

    refactor: reuse methods

--- src/zod-array-faker.ts
+++ src/zod-array-faker.ts
@@ -5,14 +5,11 @@ import { ZodTypeFaker } from './zod-type-faker'
 
 export class ZodArrayFaker<T extends z.ZodArray<any, any>> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
-    let min = this.schema._def.exactLength?.value ?? this.schema._def.minLength?.value ?? 0
-    let max =
+    const min = this.schema._def.exactLength?.value ?? this.schema._def.minLength?.value ?? 0
+    const max =
       this.schema._def.exactLength?.value ??
       this.schema._def.maxLength?.value ??
       runFake(faker => faker.number.int({ min, max: min + 2 }))
-
-    return Array(runFake(faker => faker.number.int({ min, max })))
-      .fill(null)
-      .map(() => fake(this.schema._def.type))
+    return runFake(faker => faker.helpers.multiple(() => fake(this.schema._def.type), { count: { min, max } }))
   }
 }
