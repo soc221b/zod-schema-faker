79f4c1cbf6290fe51a43993a517dd04a46dcea1d
Ernest <soc221b.e@gmail.com>
2025-01-11 11:13:04 +0800

    style: format

--- src/zod-default-faker.ts
+++ src/zod-default-faker.ts
@@ -5,8 +5,10 @@ import { ZodTypeFaker } from './zod-type-faker'
 
 export class ZodDefaultFaker<T extends z.ZodDefault<any>> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
-    return runFake(faker => faker.datatype.boolean())
-      ? this.schema._def.defaultValue()
-      : fake(this.schema._def.innerType)
+    if (runFake(faker => faker.datatype.boolean())) {
+      return this.schema._def.defaultValue()
+    } else {
+      return fake(this.schema._def.innerType)
+    }
   }
 }
