bae695c6531b09587c1c8f8166f5b51d7dcbaf16
Ernest <soc221b.e@gmail.com>
2025-01-11 11:43:09 +0800

    refactor: simplify

--- src/zod-lazy-faker.ts
+++ src/zod-lazy-faker.ts
@@ -1,20 +1,9 @@
 import { z } from 'zod'
-import { ZodSchemaFakerError } from './error'
 import { fake } from './fake'
 import { ZodTypeFaker } from './zod-type-faker'
 
 export class ZodLazyFaker<T extends z.ZodTypeAny> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
-    const schema = this.schema._def.getter()
-
-    const safeCount = 10
-    let count = 0
-    do {
-      try {
-        return fake(schema)
-      } catch {}
-    } while (++count < safeCount)
-
-    throw new ZodSchemaFakerError('Unable to generate valid values for Zod schema: ' + this.schema.toString())
+    return fake(this.schema._def.getter())
   }
 }
