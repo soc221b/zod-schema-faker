63d02469e76b36ef3dbb5ee3341ac7afafa877ad
Ernest <soc221b.e@gmail.com>
2025-01-11 11:25:14 +0800

    fix: set should sometimes generate a set which size equals min

--- src/zod-set-faker.ts
+++ src/zod-set-faker.ts
@@ -5,19 +5,15 @@ import { ZodTypeFaker } from './zod-type-faker'
 
 export class ZodSetFaker<T extends z.ZodSet<any>> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
-    let min = this.schema._def.minSize?.value ?? 0
-    let max = this.schema._def.maxSize?.value ?? runFake(faker => faker.number.int({ min, max: min + 10 }))
+    const min = this.schema._def.minSize?.value ?? 0
+    const max = this.schema._def.maxSize?.value ?? runFake(faker => faker.number.int({ min, max: min + 10 }))
 
     const set = new Set()
     while (set.size < min) {
       set.add(fake(this.schema._def.valueType))
     }
-    while (set.size < max) {
+    while (set.size < max && runFake(faker => faker.datatype.boolean())) {
       set.add(fake(this.schema._def.valueType))
-
-      if (runFake(faker => faker.datatype.boolean())) {
-        break
-      }
     }
     return set
   }
