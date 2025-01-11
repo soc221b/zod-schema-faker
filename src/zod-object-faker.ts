fd99bf2e10fcad093a43a0d46aa78ba0df97c1d7
Ernest <soc221b.e@gmail.com>
2025-01-11 12:31:31 +0800

    refactor: object generate more random data

--- src/zod-object-faker.ts
+++ src/zod-object-faker.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 import { fake } from './fake'
 import { ZodTypeFaker } from './zod-type-faker'
+import { runFake } from './random'
 
 export class ZodObjectFaker<T extends z.ZodObject<any, any, any, any, any>> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
@@ -11,11 +12,29 @@ export class ZodObjectFaker<T extends z.ZodObject<any, any, any, any, any>> exte
     if (catchall instanceof z.ZodNever) {
       switch (unknownKeys) {
         case 'passthrough': {
-          result[fake(z.string().max(10))] = fake(z.string())
+          Object.assign(
+            result,
+            Object.fromEntries(
+              runFake(faker =>
+                faker.helpers.multiple(() => [fake(z.string().regex(/^extra_[a-z]{5}$/)), fake(z.any())], {
+                  count: { min: 0, max: 5 },
+                }),
+              ),
+            ),
+          )
         }
       }
     } else {
-      result[fake(z.string().max(10))] = fake(catchall)
+      Object.assign(
+        result,
+        Object.fromEntries(
+          runFake(faker =>
+            faker.helpers.multiple(() => [fake(z.string().regex(/^extra_[a-z]{5}$/)), fake(catchall)], {
+              count: { min: 0, max: 5 },
+            }),
+          ),
+        ),
+      )
     }
 
     const shape = this.schema._def.shape()
