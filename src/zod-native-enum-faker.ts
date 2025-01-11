65216629bf4fc826b084b8d29d71b79097818522
Ernest <soc221b.e@gmail.com>
2025-01-11 11:08:06 +0800

    refactor: reuse methods

--- src/zod-native-enum-faker.ts
+++ src/zod-native-enum-faker.ts
@@ -2,19 +2,8 @@ import { z } from 'zod'
 import { runFake } from './random'
 import { ZodTypeFaker } from './zod-type-faker'
 
-const getValidEnumValues = (obj: any) => {
-  const validKeys = Object.keys(obj).filter((k: any) => typeof obj[obj[k]] !== 'number')
-  const filtered: any = {}
-  for (const k of validKeys) {
-    filtered[k] = obj[k]
-  }
-  return Object.keys(filtered).map(key => filtered[key])
-}
-
 export class ZodNativeEnumFaker<T extends z.ZodNativeEnum<any>> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
-    const values = this.schema._def.values
-    const validEnumValues = getValidEnumValues(values)
-    return runFake(faker => faker.helpers.arrayElement(validEnumValues))
+    return runFake(faker => faker.helpers.enumValue(this.schema._def.values))
   }
 }
