edafc01f0e6f594af78070ea17fa15a4b5bb138c
Ernest <soc221b.e@gmail.com>
2025-01-28 13:36:13 +0800

    refactor: extract methods

--- src/zod-intersection-faker.ts
+++ src/zod-intersection-faker.ts
@@ -7,6 +7,39 @@ export class ZodIntersectionFaker<T extends z.ZodIntersection<any, any>> extends
     const leftSchema: z.ZodType = this.schema._def.left
     const rightSchema: z.ZodType = this.schema._def.right
 
-    return leftSchema instanceof z.ZodAny || leftSchema instanceof z.ZodUnknown ? fake(rightSchema) : fake(leftSchema)
+    const fakes = [this.fakeIfOneIsAny, this.fakeIfOneIsUnknown]
+
+    for (const fakeFn of fakes) {
+      const [isSuccess, value] = fakeFn(leftSchema, rightSchema)
+      if (isSuccess) {
+        return value as z.infer<T>
+      }
+    }
+
+    throw new SyntaxError('ZodIntersectionFaker: unable to fake the given schema')
+  }
+
+  private fakeIfOneIsAny = <L extends z.ZodType, R extends z.ZodType>(
+    left: L,
+    right: R,
+  ): [boolean, null | z.infer<L> | z.infer<R>] => {
+    if (left instanceof z.ZodAny === false && right instanceof z.ZodAny === false) {
+      return [false, null]
+    }
+
+    const schema = left instanceof z.ZodAny ? right : left
+    return [true, fake(schema)]
+  }
+
+  private fakeIfOneIsUnknown = <L extends z.ZodType, R extends z.ZodType>(
+    left: L,
+    right: R,
+  ): [boolean, null | z.infer<L> | z.infer<R>] => {
+    if (left instanceof z.ZodUnknown === false && right instanceof z.ZodUnknown === false) {
+      return [false, null]
+    }
+
+    const schema = left instanceof z.ZodUnknown ? right : left
+    return [true, fake(schema)]
   }
 }
