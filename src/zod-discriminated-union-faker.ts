15aae1d9b2a26faabf81c231ef5311c9ba0ce8d3
Ernest <soc221b.e@gmail.com>
2025-02-01 08:33:40 +0800

    refactor: options can only be an array

--- src/zod-discriminated-union-faker.ts
+++ src/zod-discriminated-union-faker.ts
@@ -5,12 +5,7 @@ import { ZodTypeFaker } from './zod-type-faker'
 
 export class ZodDiscriminatedUnionFaker<T extends z.ZodDiscriminatedUnion<any, any>> extends ZodTypeFaker<T> {
   fake(): z.infer<T> {
-    const options = Array.from(
-      typeof this.schema._def.options.values === 'function'
-        ? this.schema._def.options.values()
-        : this.schema._def.options.values,
-    )
-    const randomSchema = runFake(faker => faker.helpers.arrayElement(options))
+    const randomSchema = runFake(faker => faker.helpers.arrayElement(this.schema.options))
     return fake(randomSchema as any)
   }
 }
