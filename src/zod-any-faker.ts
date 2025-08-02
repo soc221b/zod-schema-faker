a4f751b3f77bad2ad84d3a7481cdf5d20cb75a1a
Ernest <soc221b.e@gmail.com>
2025-08-02 19:11:49 +0800

    fix: generate JSON-compatible data for z.any
    fix: #520

--- src/zod-any-faker.ts
+++ src/zod-any-faker.ts
@@ -4,38 +4,11 @@ import { getFaker } from './random'
 import { ZodTypeFaker } from './zod-type-faker'
 
 const schemas = (() => {
-  const Person = z.object({
-    name: z.string(),
-  })
-  const Employee = z.object({
-    role: z.string(),
-  })
-
   const simpleSchemas = [
     z.string(),
     z.number(),
-    z.bigint(),
     z.boolean(),
-    z.date(),
-    z.undefined(),
     z.null(),
-    z.void(),
-    // z.any(),
-    z.unknown(),
-    // z.never(),
-    // z.nan(), // https://github.com/colinhacks/zod/blob/v3.24.1/src/types.ts#L3405
-    z.enum(['Salmon', 'Tuna', 'Trout']),
-    z.nativeEnum({
-      Apple: 'apple',
-      Banana: 'banana',
-      Cantaloupe: 3,
-    } as const),
-    z.optional(z.string()),
-    z.nullable(z.string()),
-    z.intersection(Person, Employee),
-    z.union([Person, Employee]),
-    z.tuple([z.string(), z.number()]),
-    z.promise(z.string()),
   ]
   const arraySchemas = simpleSchemas.map(schema => z.array(schema))
   const objectSchemas = simpleSchemas.map(schema =>
@@ -43,11 +16,8 @@ const schemas = (() => {
       foo: schema,
     }),
   )
-  const recordSchemas = simpleSchemas.map(schema => z.record(z.string(), schema))
-  const mapSchemas = simpleSchemas.map(schema => z.map(z.string(), schema))
-  const setSchemas = simpleSchemas.map(schema => z.set(schema))
 
-  return [...simpleSchemas, ...arraySchemas, ...objectSchemas, ...recordSchemas, ...mapSchemas, ...setSchemas]
+  return [...simpleSchemas, ...arraySchemas, ...objectSchemas]
 })()
 
 export class ZodAnyFaker extends ZodTypeFaker<z.ZodAny> {
