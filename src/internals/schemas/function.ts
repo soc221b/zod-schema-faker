86616bb07d01a02416a5c666e1e5a4e035ae6d2c
Ernest <soc221b.e@gmail.com>
2025-09-20 20:28:04 +0800

    feat: function

---
+++ src/internals/schemas/function.ts
@@ -0,0 +1,15 @@
+import * as core from 'zod/v4/core'
+import { Context } from '../context'
+import { rootFake as internalFake } from '../fake'
+import { Infer } from '../type'
+
+export function fakeFunction<T extends core.$ZodFunction>(
+  schema: T,
+  context: Context,
+  rootFake: typeof internalFake,
+): Infer<T> {
+  return (...args: Infer<T['_zod']['def']['input']>): Infer<T['_zod']['def']['output']> => {
+    schema.input(args)
+    return rootFake(schema._zod.def.output, context) as Infer<T['_zod']['def']['output']>
+  }
+}
